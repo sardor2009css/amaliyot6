ddae2d3a2f995f1a380c75758988c312c58fa0ba
sardor2009css <sardortoshtemirov621@gmail.com>
2025-08-28 20:27:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let bgColor = prompt("Background color kirgizing");
+ let textColor = prompt("Text color  kirgizing");
+
+ let matn = document.getElementById("matn");
+
+ matn.style.backgroundColor = bgColor;
+ matn.style.color = textColor;
